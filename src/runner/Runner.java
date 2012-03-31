78882a959ffef066efa8ac6750ed6bef8ece845b
Sergei <serjoga-zavitaev@rambler.ru>
2012-04-01 01:51:18 +0300

    Runner

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -1,14 +1,9 @@
+package runner;
 
 public class Runner
 {
-
-    /**
-     * @param args
-     */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
 
     }
-
 }
