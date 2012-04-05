d355847846b1f7d6de12b2476d152898e33805ef
Sergei <serjoga-zavitaev@rambler.ru>
2012-04-05 23:32:26 +0300

    XMLProperties interface add

---
+++ src/constants/XMLProperties.java
@@ -0,0 +1,18 @@
+package constants;
+
+public interface XMLProperties
+{
+    String ELEMENT          = "element";
+    String TYPE             = "type";
+    String FUNCTION         = "function";
+    String INFO_INPUT       = "infoInput";
+    String ADDRESS_INPUT    = "addressInput";
+    String CONTROL_INPUT    = "controlInput";
+    String DELAY            = "delay";
+    String EXTERNAL_CONTACT = "external_contact";
+    String CHAIN            = "chain";
+    String VALUE            = "value";
+    String NUMBER_CONTACT   = "numberContact";
+    String NUMBER           = "number";
+    String NUMBER_ELEMENT   = "numberElement";
+}
