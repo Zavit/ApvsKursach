64027f7882810828fb1e52e0e0dc40d9170870e2
Sergei <serjoga-zavitaev@rambler.ru>
2012-04-08 02:23:35 +0300

    new Paths add to interface

--- src/constants/Paths.java
+++ src/constants/Paths.java
@@ -2,7 +2,8 @@ package constants;
 
 public interface Paths
 {
-String XML_PATH = "resources/model.xml";
-String XML_SCHEMA_PATH = "resources/schemaModel.xsd";
+String XML_MODEL_PATH = "resources/model.xml";
+String XML_MODEL_SCHEMA_PATH = "resources/schemaModel.xsd";
 String XML_LIBRARY_PATH = "resources/Library.xml";
+String XML_LIBRARY_SCHEMA_PATH = "resources/schemaLib.xsd";
 }
